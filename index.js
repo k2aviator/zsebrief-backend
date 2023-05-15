5fbfc1e877eeb3aaa2aef099267f090ab26d61f7
Joel Detweiler <joel.detweiler@gmail.com>
2023-05-15 14:40:25 -0700

    Specify host IP

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const mongoURL = process.env.MONGO_CONNECTION_STRING || 'mongodb://127.0.0.1/jscript-330-week-8'
 
 mongoose.connect(mongoURL, {}).then(() => {
-  server.listen(port, () => {
-   console.log(`Server is listening on http://localhost:${port}`);
+  server.listen(port, '0.0.0.0', () => {
+    console.log(`Server is listening on http://localhost:${port}`);
   });
 });
