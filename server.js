6374f980d5c8d6feb1ebcae222f6a8894e09c3d1
Chris Smith <116120269+k2aviator@users.noreply.github.com>
2023-05-24 09:14:19 -0700

    added cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
 const routes = require("./routes");
+const cors = require('cors')
 
 const server = express();
 server.use(express.json());
 
 server.use(routes);
+server.use(cors())
 
 module.exports = server;
 
